352d464ddaf24d68ba4f065ba83c94746a1efea7
puttisit <47592006+puttisit@users.noreply.github.com>
2024-05-09 15:31:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function checkAnswer(answer) {
     const result = document.getElementById('results');
     const result3 = document.getElementById('result3');
     if (answer === 'c') {
-        result.textContent = "correct";
+        result.textContent = "ถูกต้องจ้า เค้าแมวทั้งคู่";
         result.style.color = 'green';
         count++;
     } else {
-        result.textContent = "incorrect";
+        result.textContent = "ไม่ถูกจ้า นู้นก็เต้าแมว";
         result.style.color = 'red';
     }
     result3.textContent = "คุณได้คะแนน " + count + " คะแนน"
