1e0657ab78287945b414756167ec1b838f3c9695
puttisit <47592006+puttisit@users.noreply.github.com>
2024-05-09 15:23:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+let count = 0; 
+
 function checkAnswer(answer) {
     const result = document.getElementById('result');
     if (answer === 'c') {
         result.textContent = "ถูกต้อง ทีม AIM อยู่ภายใต้ service Desktop Service";
         result.style.color = 'green';
+        count++;
     } else {
         result.textContent = "ผิดจ้า  ลองใหม่";
         result.style.color = 'red';
@@ -11,12 +14,15 @@ function checkAnswer(answer) {
 
  function checkAnswer2(answer) {
     const result = document.getElementById('results');
+    const result3 = document.getElementById('result3');
     if (answer === 'c') {
-        result.textContent = "ถูกต้องจ้า เค้าเเมวเหมือนกัน";
+        result.textContent = "correct";
         result.style.color = 'green';
+        count++;
     } else {
-        result.textContent = "ไม่จ้า นู้นก็เค้าแมว";
+        result.textContent = "incorrect";
         result.style.color = 'red';
     }
+    result3.textContent = "คุณได้คะแนน " + count + " คะแนน"
  }
  
